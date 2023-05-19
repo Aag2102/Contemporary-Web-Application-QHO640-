7399f6e1153c8dfe4baa447da5c45b42ef64d231
Aag2102 <Aag2102@mail.com>
2023-05-19 12:43:08 +0530

    Adding Backend API

---
+++ php/comm.php
@@ -0,0 +1,15 @@
+<?php
+
+define("DATABASE","project_uni_gym");
+define("DB_PREFIX","gymfit_");
+define("HOST","localhost");
+define("PASSWORD","");
+define("MSG_LIMIT",3);
+define("USER","root");
+//----------------------------------------------
+date_default_timezone_set("Europe/Warsaw");
+ini_set("display_errors",true);
+ini_set('error_reporting', E_ALL);
+//----------------------------------------------
+
+?>
